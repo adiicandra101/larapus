b9d05727c741bc24a7564204ccaa7ea66e8ac6e9
adiicandra101 <adiicandra101@gmail.com>
2021-11-12 18:55:34 -0800

    file baru

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="container">
+        <div class="row">
+            <div class="cold-md-12">
+                <div class="card">
+
+                <div class="card-header">
+                    Larapus
+</div>
+                        <div class="cary-body">
+                            Selamat datang di<strong>Larapus</strong>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
